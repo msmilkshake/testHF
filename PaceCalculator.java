a263b6cd1eab6184a286bac1c58d1f1bea9d94b7
HeyMilkshake <38050116+msmilkshake@users.noreply.github.com>
2022-10-24 11:44:08 +0100

    Update PaceCalculator.java
    Added more validations to users

--- PaceCalculator.java
+++ PaceCalculator.java
@@ -17,5 +17,15 @@ public class PaceCalculator {
 		System.out.printf("%02d:%02d/km", minutes, seconds);
 		System.out.print("\nPress ENTER to exit");
 		scanner.nextLine();
+		
+		System.out.print("Insert time in seconds: ");
+		int time = Integer.parseInt(scanner.nextLine());
+		System.out.print("Insert distance in kilometers: ");
+		double distance = Double.parseDouble(scanner.nextLine());
+		int minutes = (int)(time / distance / 60);
+		int seconds = (int)(((time / distance / 60) - minutes) * 60);
+		System.out.printf("%02d:%02d/km", minutes, seconds);
+		System.out.print("\nPress ENTER to exit");
+		scanner.nextLine();
 	}
 }
